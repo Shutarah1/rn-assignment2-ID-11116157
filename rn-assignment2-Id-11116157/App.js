a9404e867808700de24ce3481c363be52de5d003
Shutarah <ahmoomin@st.ug.edu.gh>
2024-05-27 13:45:59 +0000

    changed the text to my name is Abdul Halim Moomin
    change the backGroundcolor to rgb(255,50,89)
    
    changed fontSize to 24
    
    bolden the text Abdul Halim

--- rn-assignment2-Id-11116157/App.js
+++ rn-assignment2-Id-11116157/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Abdul Halim </Text>
+      <Text stle={styles.text}>
+        My name is <Text style={styles.boldText}>Abdul Halim Moomin</Text> 
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgb(255,50,89)',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: "bold",
+  }
 });
